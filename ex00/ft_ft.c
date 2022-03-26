471fdf330361dc8917ab1fbb69aed31c35a00cd8
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2022-03-26 20:37:07 +0700

    rush 01 test1

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -19,7 +19,7 @@ void    ft_putchar(char c)
     write(1, &c, 1);
 }
 
-char    itoa(int i)
+char    ft_itoa(int i)
 {
     char    a;
 
@@ -32,18 +32,26 @@ void error(void)
     write(1, "Error\n", 6);
 }
 
+int ft_atoi(char c)
+{
+    int i;
+    
+    i = c - 48;
+    return (i);
+}
+
 void print_map(int **map)
 {
     int i;
     int j;
 
     i = -1;
-    while (++i < array_size(&map[0]))
+    while (++i < array_size(&map[0][0]))
     {
         j = -1;
-        while (++j < array_size(&map[0]))
+        while (++j < array_size(&map[0][0]))
         {
-            ft_putchar(itoa(map[i][j]));
+            ft_putchar(ft_itoa(map[i][j]));
             ft_putchar(' ');
         }
         ft_putchar('\n');
--- ex00/rush01.c
+++ ex00/rush01.c
@@ -1,8 +1,47 @@
+#include <stdlib.h>
+#include "ft_ft.c"
+
+int *split_input(char *str)
+{
+    int *array; 
+    int i;
+    int j;
+    //col1up col2up col3up col4up col1down col2down col3down col4down row1left row2left row3left row4left row1right row2right row3right row4right
+    i = 0;
+    j = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == ' ')
+        {
+            j++;
+        }
+        i++;
+    }
+    array = (int *) malloc(sizeof(int) * (j + 1));
+    i = 0;
+    j = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == ' ')
+        {
+            array[j++] = ft_atoi(str[i - 1]);
+        }
+        i++;
+    }
+    array[j] = ft_atoi(str[i - 1]);
+    return (array);
+}
+
+#include <stdio.h>
 int main(int argv, char **argc)
 {
+    int *array_input;
     if (argv == 2)
     {
-        
+        array_input = split_input(&argc[1][0]);
+        for(int i=0;i<16;i++){
+            printf("%d, ", array_input[i]);
+        }
         //code here 
     }
     return (0);
