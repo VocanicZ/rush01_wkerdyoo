f224153f86b44ef65c949c36909c3cc4803bdd0b
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2022-03-27 02:07:05 +0700

    test

--- ex00/rush01.c
+++ ex00/rush01.c
@@ -2,6 +2,13 @@
 #include "ft_ft.c"
 #include <stdio.h>
 
+void    field_condition(int **board, int *condition);
+
+void    print_line(void)
+{
+    printf("-------------------------------------\n");
+}
+
 int main(int argv, char **argc)
 {
     int **board;
@@ -10,9 +17,12 @@ int main(int argv, char **argc)
     {
         array_input = split_input(&argc[1][0]);
         board = gen_board(array_size(array_input) / 4);
-
-
-        //print_map(board);
+        print_line();
+        print_board(board);
+        print_line();
+        field_condition(board, array_input);
+        print_board(board);
+        print_line();
         print_board(combine_board_ui(board, array_input));
     }
     else
