a3d518f7ba61fbc89541cb896029604ff9625d23
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2022-03-27 02:22:44 +0700

    impliment include

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -1,5 +1,8 @@
 #include <unistd.h>
-#include "ft_logic.c"
+
+int array_size(int *array);
+
+char    ft_itoa(int i);
 
 //This file contain all function using write()
 void    ft_putchar(char c)
@@ -13,22 +16,22 @@ int error(void)
     return (0);
 }
 
-void print_board(int **map)
+void print_board(int **board)
 {
     int i;
     int j;
 
     i = -1;
-    while (++i < array_size(&map[0][0]))
+    while (++i < array_size(&board[0][0]))
     {
         j = -1;
-        while (++j < array_size(&map[0][0]))
+        while (++j < array_size(&board[0][0]))
         {
-            if (map[i][j] == 0)
+            if (board[i][j] == 0)
                 ft_putchar(' ');
             else
-                ft_putchar(ft_itoa(map[i][j]));
-            if (j != array_size(&map[0][0]) - 1)
+                ft_putchar(ft_itoa(board[i][j]));
+            if (j != array_size(&board[0][0]) - 1)
                 ft_putchar(' ');
         }
         ft_putchar('\n');
--- ex00/rush01.c
+++ ex00/rush01.c
@@ -1,8 +1,18 @@
-#include <stdlib.h>
-#include "ft_ft.c"
 #include <stdio.h>
 
 void    field_condition(int **board, int *condition);
+
+int **gen_board(int n);
+
+void    print_board(int **board);
+
+int *split_input(char *str);
+
+int array_size(int *array);
+
+int **combine_board_ui(int **board, int *ui);
+
+int error(void);
 
 void    print_line(void)
 {
