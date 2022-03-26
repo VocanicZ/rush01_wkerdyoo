4c39a67b5266f6ed7244b063b00ee84c82c9a6eb
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2022-03-27 00:48:05 +0700

    basic array and ui system

--- ex00/ft_logic.c
+++ ex00/ft_logic.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
-#include <stdio.h>
+
+int error(void);
 
 int array_size(int *array)
 {
@@ -53,8 +54,96 @@ int *ft_sqrt(int n)
     {
         i++;
     }
-    tmp[0] = ft_pow(i - 1, 2);
-    if (n - tmp[0] != 0)
+    tmp[0] = i - 1;
+    if (n - ft_pow(tmp[0], 2) != 0)
         tmp[1] = 1;
     return (tmp);
+}
+
+int *split_input(char *str)
+{
+    int *array; 
+    int i;
+    int j;
+    int k;
+
+    i = 0;
+    j = 0;
+    k = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == ' ')
+            k++;
+        i++;
+    }
+    if ((k + 1) % 4 != 0)
+    {
+        error();
+        return (NULL);
+    }
+    array = (int *) malloc(sizeof(int) * (k + 2));
+    array[k + 1] = -1; 
+    i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == ' ')
+            array[j++] = ft_atoi(str[i - 1]);
+        else if (ft_atoi(str[i]) > k / 4 || ft_atoi(str[i]) == 0)
+        {
+            error();
+            return (NULL);
+        }
+        i++;
+    }
+    array[j] = ft_atoi(str[i - 1]);
+    return (array);
+}
+
+int **gen_board(int n)
+{
+    int i;
+    int j;
+    int **board;
+
+    i = 0;
+    board = (int **) malloc(sizeof(int *) * (n));
+    while (i < n)
+    {
+        board[i] = (int *) malloc(sizeof(int) * (n + 1));
+        j = 0;
+        while (j < n)
+            board[i][j++] = 0;
+        board[i++][n] = -1;
+    }
+    return (board);
+}
+
+int **combine_board_ui(int **board, int *ui)
+{
+    int **ui_board;
+    int i;
+    int j;
+    int pos[3];
+
+    ui_board = gen_board(array_size(ui) / 4 + 2);
+    i = -1;
+    pos[0] = 0;
+    pos[1] = array_size(ui) / 2;
+    pos[2] = array_size(ui) * 3 / 4;
+    while (++i < array_size(ui) / 4 + 2)
+    {
+        j = -1;
+        while (++j < array_size(ui) / 4 + 2)
+        {
+            if ((i == 0 || i == array_size(ui) / 4 + 1) && j > 0 && j < array_size(ui) / 4 + 1)
+                ui_board[i][j] = ui[pos[0]++];
+            else if (j == 0 && i > 0 && i < array_size(ui) / 4 + 1)
+                ui_board[i][j] = ui[pos[1]++];
+            else if (j == array_size(ui) / 4 + 1 && i > 0 && i < array_size(ui) / 4 + 1)
+                ui_board[i][j] = ui[pos[2]++];
+            else if ((i > 0 && i < array_size(ui) / 4 + 1) && j > 0 && j < array_size(ui) / 4 + 1)
+                ui_board[i][j] = board[i - 1][j - 1];
+        }
+    }
+    return (ui_board);
 }
--- ex00/rush01.c
+++ ex00/rush01.c
@@ -2,49 +2,24 @@
 #include "ft_ft.c"
 #include <stdio.h>
 
-int *split_input(char *str)
-{
-    int *array; 
-    int i;
-    int j;
-    //col1up col2up col3up col4up col1down col2down col3down col4down row1left row2left row3left row4left row1right row2right row3right row4right
-    i = 0;
-    j = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] == ' ')
-            j++;
-        i++;
-    }
-    if (ft_sqrt(j)[1] != 1 || j < 3)
-    {
-        error();
-        return (NULL);
-    }
-    array = (int *) malloc(sizeof(int) * (j + 1));
-    array[j + 1] = -1; 
-    i = 0;
-    j = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] == ' ')
-            array[j++] = ft_atoi(str[i - 1]);
-        i++;
-    }
-    array[j] = ft_atoi(str[i - 1]);
-    return (array);
-}
-
-
 int main(int argv, char **argc)
 {
+    int **board;
+    int **ui_board;
     int *array_input;
     if (argv == 2)
     {
+        printf("run\n");
         array_input = split_input(&argc[1][0]);
-        for(int i=0;i<array_size(array_input);i++)
-            printf("%d, ", array_input[i]);
-        //code here 
+        printf("after input strip\n");
+        board = gen_board(array_size(array_input) / 4);
+        printf("b4 print board\n");
+        print_map(board);
+        printf("b4 draw ui\n");
+        ui_board = combine_board_ui(board, array_input);
+        printf("after draw ui\n");
+        print_map(ui_board);
+        printf("done\n");
     }
     else
         return (error());
