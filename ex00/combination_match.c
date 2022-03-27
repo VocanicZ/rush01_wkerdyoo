fc1763a52bc92bdafe9cdc16bf982e6c40f81156
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2022-03-27 13:02:52 +0700

    retunr 2 if its no more comdition

--- ex00/combination_match.c
+++ ex00/combination_match.c
@@ -1,6 +1,33 @@
-int combination_match(int **map, int **combination, int i)
+int array_size(int *array);
+
+int **gen_combination(int first, int last, int n);
+
+int combination_match(int *array, int first, int last, int n)
 {
+    int i;
+    int j;
+    int **combination;
     //match each row with 1st combination that cat fit with **map
-    //return 1 if there is an error ex. no match combination
+    i = -1;
+    j = 0;
+    combination = gen_combination(first, last, array_size(array)); 
+    while (combination[i][0] != 0)
+        j++;
+    if (n > j)
+        return (2);
+    i = -1;
+    while (++i < array_size(array))
+    {
+        if (!(array[i] == 0 || array[i] == combination[n][i]))
+            j++;
+    }
+    if (j != array_size(array)) 
+        return (1);
+    i = -1;
+    while (++i < array_size(array))
+    {
+        if (array[i] == 0)
+            array[i] = combination[n][i];
+    }
     return (0);
 }
