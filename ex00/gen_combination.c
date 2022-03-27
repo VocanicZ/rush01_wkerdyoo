7c3afbf87fe608a9890ce4dfb4223d52ba16ad99
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2022-03-27 07:52:34 +0700

    almost done

--- ex00/gen_combination.c
+++ ex00/gen_combination.c
@@ -1,4 +1,58 @@
-void    ft_gen(char i, char j, char k, char l)
-{
+#include <stdlib.h>
+#include <stdio.h>
 
+int array_size(int *array);
+
+int *hash_array(int *array);
+
+void    ft_swap(int *a, int *b);
+
+void    append_array(int **array, int *p);
+
+int *ft_copy(int *array);
+
+int ft_factorial(int n);
+
+void    print_array(int *array)
+{
+    for (int i = 0; i<array_size(array);i++)
+    {
+        printf("%d ",array[i]);
+    }
+    printf("\n");
+}
+
+void    gen_all_combination(int **big_array, int *array, int index)
+{
+    int j;
+    int *tmp;
+
+    if (index == array_size(array) - 1)
+        append_array(big_array, array);
+    j = index - 1;
+    while (++j < array_size(array))
+    {
+        tmp = ft_copy(array);
+        ft_swap(&tmp[index], &tmp[j]);
+        gen_all_combination(big_array, tmp, index + 1);
+    }
+}
+
+int    **gen_combination(int first, int last, int n)
+{
+    int **all_combination;
+    int *gen;
+    int *hash;
+    int i;
+
+    i = -1;
+    gen = (int *) malloc(sizeof(int) * (n + 1));
+    gen[n] = -1;
+    while (++i < n)
+        gen[i] = i + 1;
+    all_combination = (int **) malloc(sizeof(int *) * (ft_factorial(n) + 1));
+    all_combination[0] = (int *) malloc(sizeof(int) * (array_size(gen) + 1));
+    all_combination[0][0] = -1;
+    gen_all_combination(all_combination, gen, 0);
+    return (all_combination);
 }
