45ff2888ce55952519f176de70eca76989272f47
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2022-03-27 14:07:42 +0700

    fix malloc

--- ex00/check_dupliclate.c
+++ ex00/check_dupliclate.c
@@ -1,24 +1,24 @@
-static int **board;
+#include <stdlib.h>
 
 int  *checkDuplicate(int size, int **board) {
         int i;
         int j;
         int k;
+        int *result;
 
         i = 0;
         j = 0;
         k = 1;
+        result = (int *) malloc(sizeof(int) * 3);
+        result[2] = -1;
         while (i < size) {
             while (j < size) {
                 while (k < size - 1) {
-                    // using for row loop 
-                    if (board[i][j] == board[i][k]) {
-
-                        return board[i][board[i][k]];
-                    }
-                    //using for col loop
-                    if (board[i][j] == board[k][i]) {
-                        return board[i][board[i][k]];
+                    // using for row, col loop 
+                    if (board[i][j] == board[i][k] || board[i][j] == board[k][j]) {
+                        result[0] = i;
+                        result[1] = board[i][k];
+                        return result;
                     }
                     k++;
                 }
